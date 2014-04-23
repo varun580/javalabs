c439e35cfa5ea1738afc12d30e78282793cc2d67
varun580 <varun580@gmail.com>
2014-04-23 08:43:13 +0530

    yy

---
+++ javaLab/asci.java
@@ -0,0 +1,15 @@
+
+public class asci {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = Character.toString((char)63);
+		System.out.println("varun"+s+"reddy");
+		String s1 = "vaqrun"+"?";
+		System.out.println(s1);
+	}
+
+}
