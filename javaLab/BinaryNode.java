3aca543926225f4ad6c5e929c5e7c89d7dd01f74
varun580 <varun580@gmail.com>
2014-04-23 08:32:30 +0530

    first commit

---
+++ javaLab/BinaryNode.java
@@ -0,0 +1,31 @@
+
+
+public class BinaryNode {
+
+BinaryNode()
+{
+	this.data = null;
+	this.left = null;
+	this.right = null;	 
+}
+	
+ BinaryNode(Object Data)
+ {
+	this.data = Data;
+	this.left = null;
+	this.right = null;	   
+ }
+
+ BinaryNode( Object Data, BinaryNode left, BinaryNode right )
+ {
+ 	this.data = Data;
+ 	this.left = left;
+ 	this.right = right;	 
+ }
+
+ 
+ 
+ Object data;	
+ BinaryNode left;
+ BinaryNode right;
+}
---
+++ javaLab/Tree.java
@@ -0,0 +1,15 @@
+
+public class Tree {
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BinaryNode b = new BinaryNode("d"); 
+	}
+
+}
